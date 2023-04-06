cb2c6b3085d34fc207f15ad82a9acb1b7267cef9
Nikita Kalmykov <nik.kalmykov.1986@mail.ru>
2023-04-06 20:35:23 +0300

    changes

--- src/modules/TasksError/TasksError.module.tsx
+++ src/modules/TasksError/TasksError.module.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { Link, useNavigate } from 'react-router-dom';
-import sad_cat from '../../assets/sad_cat.webp';
+import sad_cat from 'assets/sad_cat.webp';
 import { PATH_LIST } from 'constants/index';
 import { PageContainer } from 'components/index';
 
