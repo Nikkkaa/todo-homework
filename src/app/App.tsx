3f4eec41ed71c1e9a149d3e4a8be7f4dcd2bd196
Nikita Kalmykov <nik.kalmykov.1986@mail.ru>
2023-04-06 23:17:25 +0300

    cssbaseline

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from '@mui/material';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from 'router/index';
@@ -5,6 +6,7 @@ import { Router } from 'router/index';
 export function App() {
   return (
     <BrowserRouter>
+      <CssBaseline />
       <Router />
     </BrowserRouter>
   );
